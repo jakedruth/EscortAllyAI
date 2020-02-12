b805de74f9380991c9e257b9d4e939f6ca4dbaa2
jacob.ruth <Jacob.ruth@mymail.champlain.edu>
2020-02-12 11:52:15 -0500

    Added the AllyEscort data

---
+++ Assets/AllyEscort/Scripts/EscortAgent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AllyEscort
+{
+    public class EscortAgent : MonoBehaviour
+    {
+        [Header("Variables")]
+        public float speed;
+        public float acceleration;
+        public string otherVariables;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/AllyEscort/Scripts/EscortMessenger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AllyEscort
+{
+    public class EscortMessenger : MonoBehaviour
+    {
+        public EscortAgent escortAgent;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
