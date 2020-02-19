8da56edc1a1baf6647b820013c9f6a19b3ffb841
Jakedruth@gmail.com <Jakedruth@gmail.com>
2020-02-19 13:38:14 -0500

    Created Empty State

---
+++ Assets/AllyEscort/Scripts/States/EmptyState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyState : ScriptableObject
+{
+    public virtual void OnEnter() { }
+    public virtual void Update() { }
+    public virtual void OnExit() { }
+}
