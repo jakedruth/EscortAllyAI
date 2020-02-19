689c0619957c8af6a8d768988383b313ce1a2012
Jake Ruth <jakedruth@users.noreply.github.com>
2020-02-19 10:47:04 -0500

    work on scriptable objects

---
+++ Assets/AllyEscort/Scripts/States/CreateState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateState
+{
+    [UnityEditor.MenuItem("Ally Escort/State", menuItem = "State/CreateNewState", priority = 1)]
+    public static void Test() 
+    { }
+}
