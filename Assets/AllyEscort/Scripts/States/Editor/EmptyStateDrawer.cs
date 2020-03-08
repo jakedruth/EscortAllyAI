c58fb5113ed951b650e1b9326197f1f753c5a596
Jakedruth@gmail.com <Jakedruth@gmail.com>
2020-03-07 21:07:00 -0500

    Completed Generate New Behavior State

--- Assets/AllyEscort/Scripts/States/Editor/EmptyStateDrawer.cs
+++ Assets/AllyEscort/Scripts/States/Editor/EmptyStateDrawer.cs
@@ -1,41 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using NUnit.Framework.Constraints;
 using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace AllyEscort
 {
     [CustomEditor(typeof(EmptyState))]
     public class EmptyStateDrawer : Editor
     {
-        public EmptyState EmptyStateTarget { get; private set; }
 
         public void OnEnable()
         {
-            EmptyStateTarget = (EmptyState) target;
+            //EmptyStateTarget = (EmptyState) target;
         }
 
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
             EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
-            if (GUILayout.Button("Generate Custom State"))
+            if (GUILayout.Button("Generate State Behavior"))
             {
-                //Debug.Log(Selection.activeObject.name);
+                // Get the selected object (the scriptable object that wants a custom state behavior)
                 Object selectedObject = Selection.activeObject;
                 string path = AssetDatabase.GetAssetPath(selectedObject);
                 string text = File.ReadAllText(path);
 
+                // Prompt a save file dialog and get a path to it
                 string newFilePath = EditorUtility.SaveFilePanel("Create Behavior Script",
                     path, 
-                    Path.GetFileNameWithoutExtension(path),
+                    Path.GetFileNameWithoutExtension(path) + "State",
                     "cs");
-
-                string fileName = Path.GetFileNameWithoutExtension(newFilePath);
-
+                
+                // null if user clicks cancel
                 if (newFilePath != null)
                 {
+                    string fileName = Path.GetFileNameWithoutExtension(newFilePath);
+
+                    // create file from template
                     File.WriteAllText(newFilePath,
                         $@"using System.Collections;
 using System.Collections.Generic;
@@ -45,8 +50,10 @@ namespace AllyEscort
 {{
     public class {fileName} : State
     {{
-        internal override void HandleInitialize()
-        {{ }}
+        internal override bool HandleInitialize()
+        {{
+            return true;
+        }}
 
         internal override void HandleOnEnter()
         {{ }}
@@ -59,28 +66,52 @@ namespace AllyEscort
     }}
 }}");
                     
+                    // Used to reload the editor
                     AssetDatabase.SaveAssets();
                     AssetDatabase.Refresh();
 
-                    string relativePath = newFilePath.Substring(newFilePath.IndexOf("Assets"));
-                    Debug.Log(relativePath);
-
+                    // Load the newly created file from a relative path
+                    string relativePath = newFilePath.Substring(newFilePath.IndexOf("Assets", StringComparison.Ordinal));
                     Object newFile = AssetDatabase.LoadAssetAtPath<Object>(relativePath);
-                    Debug.Log(newFile.GetType());
 
+                    // Get the new file's guid and localID
+                    if (AssetDatabase.TryGetGUIDAndLocalFileIdentifier(newFile, out string guid, out long localId))
+                    {
+                        // read the selected objects text to get the element "m_Script"
+                        string scriptElementName = "m_Script";
+                        int scriptElementStartIndex = text.IndexOf(scriptElementName, StringComparison.Ordinal);
+
+                        // Extract the fileID
+                        int fileIDIndexStart = text.IndexOf("fileID: ", scriptElementStartIndex, StringComparison.Ordinal) + 8; // manually offset index by string length of "fileID: " 
+                        int fileIDIndexEnd = text.IndexOf(',', fileIDIndexStart);
+                        string fileIDCurrent = text.Substring(fileIDIndexStart, fileIDIndexEnd - fileIDIndexStart);
+
+                        // Extract the guid
+                        int guidIndexStart = text.IndexOf("guid: ", scriptElementStartIndex, StringComparison.Ordinal) + 6;     // manually offset index by string length of "guid: " 
+                        int guidIndexEnd = text.IndexOf(',', guidIndexStart);
+                        string guidCurrent = text.Substring(guidIndexStart, guidIndexEnd - guidIndexStart);
+
+
+                        // Replace the selected objects guid and fileId with the new ones
+                        text = text.Replace(fileIDCurrent, localId.ToString());
+                        text = text.Replace(guidCurrent, guid);
+
+                        // ReWrite the new text of the scriptable object to the path
+                        File.WriteAllText(path, text);
+
+                        // Reload the editor
+                        AssetDatabase.SaveAssets();
+                        AssetDatabase.Refresh();
+
+                        Selection.activeObject = selectedObject;
+                    }
+
+                    // Attempt to show the new object in the project tab
                     EditorGUIUtility.PingObject(newFile);
-
                     EditorUtility.FocusProjectWindow();
+
+                    AssetDatabase.OpenAsset(newFile);
                 }
-
-                // TODO:
-                // 1) Create Template for ScriptableObject of State
-                // 2) create file
-                // 3) get new file's GUID
-                // 4) Switch selectedObject's file with new file
-                // 5) ???
-                // 6) Profit
-
             }
         }
     }
